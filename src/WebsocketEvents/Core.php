e0a1b5715088dcf129cc2a81936da6e7522c0688
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2023-05-07 23:34:26 +0200

    Just forgot to Ctrl+S

--- src/WebsocketEvents/Core.php
+++ src/WebsocketEvents/Core.php
@@ -88,12 +88,12 @@ class Core extends PluginBase {
                     continue;
                 }
 
-                if (stripos($messgae, "HTTP/1.1") !== false) {
+                echo "Received message! - {$message}\n";
+
+                if (stripos($message, "HTTP/1.1") !== false) {
                     echo "Connected!\n";
                     continue;
                 }
-
-                echo "Received message! - {$message}\n";
                 // Send a command to the console.
                 // WAIT! Let's see the type! if it's get SO we need to send the user's informations!
                 $message = json_decode($message);
