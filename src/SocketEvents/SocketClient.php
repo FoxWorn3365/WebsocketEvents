6a60d3527a6b547e05ccecf0baca4720c8051164
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2023-05-08 19:20:07 +0200

    Added mask and unmask for th

--- src/SocketEvents/SocketClient.php
+++ src/SocketEvents/SocketClient.php
@@ -17,11 +17,44 @@ class SocketClient {
         $this->message = $callback;
     }
 
+    protected function unmask(string $text) : string {
+        $length = ord($text[1]) & 127;
+        if ($length == 126) {
+            $masks = substr($text, 4, 4);
+            $data = substr($text, 8);
+        } elseif ($length == 127) {
+            $masks = substr($text, 10, 4);
+            $data = substr($text, 14);
+        } else {
+            $masks = substr($text, 2, 4);
+            $data = substr($text, 6);
+        }
+        $text = "";
+        for ($i = 0; $i < strlen($data); ++$i) {
+            $text .= $data[$i] ^ $masks[$i % 4];
+        }
+        return $text;
+    }
+
+    protected function mask(string $text) : string {
+        $b1 = 0x80 | (0x1 & 0x0f);
+        $length = strlen($text);
+    
+        if ($length <= 125)
+            $header = pack('CC', $b1, $length);
+        elseif ($length > 125 && $length < 65536)
+            $header = pack('CCn', $b1, 126, $length);
+        elseif ($length >= 65536)
+            $header = pack('CCNN', $b1, 127, $length);
+        return $header . $text;
+    }
+
     public function read(int $lenght = 1024) : ?string {
-        return socket_read($this->client, $lenght);
+        return $this->unmask(socket_read($this->client, $lenght, PHP_BINARY_READ));
     }
 
     public function write(string $message) : void {
+        $message = $this->mask($message);
         if (!socket_send($this->client, $message, strlen($message), 0)) {
             $this->close();
         }
